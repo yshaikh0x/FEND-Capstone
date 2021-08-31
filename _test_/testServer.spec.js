bbb97e27cbaa3242dee6efc92a114932dc45598e
unknown <yshaikh0x@gmail.com>
2021-08-30 23:45:54 -0400

    working jest tests

--- _test_/testServer.spec.js
+++ _test_/testServer.spec.js
@@ -1,7 +1,10 @@
-const apiKeys = require("../src/server/apiKeys");
+const portCheck = require("../src/server/index.js");
 
-
-//test function
-test("Check geonames api key is included", () => {
-  expect(apiKeys('geonamesApi')).not.toBe('');
-});
+ // The describe() function  
+ describe("check port", () => {
+  // The test() function 
+  test("Testing if the port number is included", () => {
+         // Define the input for the function, if any, in the form of variables/array
+         // Define the expected output, if any, in the form of variables/array
+         expect(portCheck).toBeDefined();
+})});
